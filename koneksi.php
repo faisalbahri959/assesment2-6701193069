c11da35ed7fed42add06a61d746a91a630f164c1
faisalbahri959 <60419977+faisalbahri959@users.noreply.github.com>
2021-05-05 21:41:28 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // konfigurasi database
-$server = 'localhost';
-$user = 'root';
-$password = '';
-$nama_database = 'tubes';
+$server = 'pabw-4303-assesment2.mysql.database.azure.com';
+$user = 'FaisalBahri@pabw-4303-assesment2';
+$password = 'Mytana230175';
+$nama_database = 'pabw';
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 if (!$db) {
